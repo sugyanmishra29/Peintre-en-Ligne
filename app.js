a18c67e7c816c7b6b9a148adeb6c9f0571f665b0
sugyanmishra29 <sugyanmishra.sinu@gmail.com>
2020-09-08 00:22:02 +0530

    changes reverted because of bugs

--- app.js
+++ app.js
@@ -75,27 +75,3 @@ function draw(e) {
     ctx.lineTo(pos.x, pos.y);
     ctx.stroke();
 }
-
-var randomColourOne;
-var randomColourTwo;
-
-$(document).ready(function() {
-    $("#colourOne").hide();
-    $("#colourTwo").hide();
-})
-
-$(document).ready(function() {
-    $("body").hover(function() {
-        $("#colourOne").fadeToggle(300);
-        $("#colourTwo").fadeToggle(300);
-    });
-});
-
-function firstColour() {
-    randomColourOne = "#000000".replace(/0/g, function() { return (~~(Math.random() * 16)).toString(16); });
-
-    randomColourTwo = "#000000".replace(/0/g, function() { return (~~(Math.random() * 16)).toString(16); });
-
-    var background = document.getElementById("background");
-    background.style.backgroundImage = "-webkit-linear-gradient(" + randomColourOne + " , " + randomColourTwo + ")";
-}
