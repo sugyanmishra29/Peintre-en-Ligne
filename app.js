5f7f0fc30f11664c9161ca52e946974f0eae1b24
sugyanmishra29 <sugyanmishra.sinu@gmail.com>
2020-09-07 23:51:42 +0530

    design changes and upload() removed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var pen = document.getElementById("pen");
 var brush = document.getElementById("brush");
 var erase = document.getElementById("erase");
 var download = document.getElementById("download");
-var upload = document.getElementById("upload");
+// var upload = document.getElementById("upload");
 resize();
 
 function resize() {
@@ -19,35 +19,35 @@ canvas.addEventListener("mouseenter", setPosition1);
 pen.addEventListener("mousedown", changePen);
 brush.addEventListener("mousedown", changebrush);
 erase.addEventListener("mousedown", resize);
-download.addEventListener("click", function(e) {
+download.addEventListener("click", function() {
     var image = canvas.toDataURL("image/png")
         .replace("image/png", "image/octet-stream");
     download.setAttribute("href", image);
 });
 
 
-upload.addEventListener('change', function(ev) {
-    if (ev.target.files) {
-        let file = ev.target.files[0];
-        var reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = function(e) {
-            var image = new Image();
-            image.src = e.target.result;
-            image.onload = function(ev) {
-                canvas.style.background = image;
-            }
-        }
-    }
-});
+// upload.addEventListener('change', function(ev) {
+//     if (ev.target.files) {
+//         let file = ev.target.files[0];
+//         var reader = new FileReader();
+//         reader.readAsDataURL(file);
+//         reader.onloadend = function(e) {
+//             var image = new Image();
+//             image.src = e.target.result;
+//             image.onload = function() {
+//                 canvas.style.background = image;
+//             }
+//         }
+//     }
+// });
 
 ctx.lineCap = "round";
 
-function changePen(e) {
+function changePen() {
     ctx.lineCap = "round";
 }
 
-function changebrush(e) {
+function changebrush() {
     ctx.lineCap = "square";
 }
 
@@ -74,4 +74,28 @@ function draw(e) {
     setPosition(e, ctx.canvas);
     ctx.lineTo(pos.x, pos.y);
     ctx.stroke();
+}
+
+var randomColourOne;
+var randomColourTwo;
+
+$(document).ready(function() {
+    $("#colourOne").hide();
+    $("#colourTwo").hide();
+})
+
+$(document).ready(function() {
+    $("body").hover(function() {
+        $("#colourOne").fadeToggle(300);
+        $("#colourTwo").fadeToggle(300);
+    });
+});
+
+function firstColour() {
+    randomColourOne = "#000000".replace(/0/g, function() { return (~~(Math.random() * 16)).toString(16); });
+
+    randomColourTwo = "#000000".replace(/0/g, function() { return (~~(Math.random() * 16)).toString(16); });
+
+    var background = document.getElementById("background");
+    background.style.backgroundImage = "-webkit-linear-gradient(" + randomColourOne + " , " + randomColourTwo + ")";
 }
